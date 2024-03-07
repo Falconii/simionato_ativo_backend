e9166ee90e20035c46b9396bd01ea82e7f98f4bd
Falconii <falconi.2050@gmail.com>
2024-03-07 10:44:42 -0300

    Versao 07 03 A

--- route/helloRoute.js
+++ route/helloRoute.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('', function(req, res) {
+
+
+router.get('/api/splash', function(req, res) {
+
+    setTimeout(function() {
+        res.status(200).json({ message: 'Sistema No Ar!' });
+    }, 3000);
+
+
+})
+
+router.get('/api/hello', function(req, res) {
 
     res.status(200).json({ message: 'Sistema No Ar!' });
 
 })
-
 module.exports = router;
