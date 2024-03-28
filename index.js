55a94b1ffa2ef451bb7965b6166a94696ba528d8
Falconii <falconi.2050@gmail.com>
2024-03-28 11:42:31 -0300

    teste 03

--- index.js
+++ index.js
@@ -1,85 +1,88 @@
-const express = require('express');
-//lixo
-const fs = require('fs');
-const parametroSrv = require('./service/parametroService');
-var os = require('os');
+const express = require("express");
+
+const fs = require("fs");
+const parametroSrv = require("./service/parametroService");
+var os = require("os");
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 
 const allowCors = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // colocar os dominios permitidos | ex: 127.0.0.1:3000
 
-    res.header("Access-Control-Allow-Origin", "*"); // colocar os dominios permitidos | ex: 127.0.0.1:3000
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials, X-Access-Token, X-Key"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, PUT, POST, DELETE, OPTIONS, PATCH"
+  );
 
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials, X-Access-Token, X-Key");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, PATCH");
+  res.header("Access-Control-Allow-Credentials", "false");
 
-    res.header("Access-Control-Allow-Credentials", "false");
-
-    next();
-}
+  next();
+};
 
 app.use(allowCors);
 
+app.use("/", require("./route/helloRoute.js"));
+app.use("/", require("./route/empresaRoute.js"));
+app.use("/", require("./route/localRoute.js"));
+app.use("/", require("./route/usuarioRoute.js"));
+app.use("/", require("./route/grupousuarioRoute.js"));
+app.use("/", require("./route/principalRoute.js"));
+app.use("/", require("./route/produtoRoute.js"));
+app.use("/", require("./route/grupoRoute.js"));
+app.use("/", require("./route/centrocustoRoute.js"));
+app.use("/", require("./route/fornecedorRoute.js"));
+app.use("/", require("./route/empresaRoute.js"));
+app.use("/", require("./route/grupoRoute.js"));
+app.use("/", require("./route/centrocustoRoute.js"));
+app.use("/", require("./route/imobilizadoRoute.js"));
+app.use("/", require("./route/imobilizadoinventarioRoute.js"));
+app.use("/", require("./route/nfeRoute.js"));
+app.use("/", require("./route/valorRoute.js"));
+app.use("/", require("./route/inventarioRoute.js"));
+app.use("/", require("./route/usuarioinventarioRoute.js"));
+app.use("/", require("./route/usuarioinventarioRoute.js"));
+app.use("/", require("./route/lancamentoRoute.js"));
+app.use("/", require("./route/parametroRoute.js"));
+app.use("/", require("./route/custom/parametroRoute.js"));
+app.use("/", require("./route/padraoRoute.js"));
+app.use("/", require("./route/LoadFileRouter.js"));
+app.use("/", require("./route/fotoRoute.js"));
 
-app.use('/', require('./route/helloRoute.js'));
-app.use('/', require('./route/empresaRoute.js'));
-app.use('/', require('./route/localRoute.js'));
-app.use('/', require('./route/usuarioRoute.js'));
-app.use('/', require('./route/grupousuarioRoute.js'));
-app.use('/', require('./route/principalRoute.js'));
-app.use('/', require('./route/produtoRoute.js'));
-app.use('/', require('./route/grupoRoute.js'));
-app.use('/', require('./route/centrocustoRoute.js'));
-app.use('/', require('./route/fornecedorRoute.js'));
-app.use('/', require('./route/empresaRoute.js'));
-app.use('/', require('./route/grupoRoute.js'));
-app.use('/', require('./route/centrocustoRoute.js'));
-app.use('/', require('./route/imobilizadoRoute.js'));
-app.use('/', require('./route/imobilizadoinventarioRoute.js'));
-app.use('/', require('./route/nfeRoute.js'));
-app.use('/', require('./route/valorRoute.js'));
-app.use('/', require('./route/inventarioRoute.js'));
-app.use('/', require('./route/usuarioinventarioRoute.js'));
-app.use('/', require('./route/usuarioinventarioRoute.js'));
-app.use('/', require('./route/lancamentoRoute.js'));
-app.use('/', require('./route/parametroRoute.js'));
-app.use('/', require('./route/custom/parametroRoute.js'));
-app.use('/', require('./route/padraoRoute.js'));
-app.use('/', require('./route/LoadFileRouter.js'));
-app.use('/', require('./route/fotoRoute.js'));
-
-app.listen(PORT, () => { console.log(`Servidor No Ar. Porta ${PORT}`); });
+app.listen(PORT, () => {
+  console.log(`Servidor No Ar. Porta ${PORT}`);
+});
 
 //atualizando a key
 
 refresh();
 
 async function refresh() {
+  let arquivo = "";
 
-    let arquivo = "";
+  //Buscando key google
+  const param = await parametroSrv.getParametro(1, "key", "googledrive", 999);
+  if (param == null) {
+    console.log("Não Foi Encontrada Chave GOOGLE DRIVE");
+    return;
+  }
+  if (PORT == 3000) {
+    arquivo =
+      "C:/Repositorios Git/Simionato/controle de ativo/keys/googlekey.json";
+  } else {
+    arquivo = "keys/googlekey.json";
+  }
 
-    //Buscando key google 
-    const param = await parametroSrv.getParametro(1, "key", "googledrive", 999);
-    if (param == null) {
-        console.log('Não Foi Encontrada Chave GOOGLE DRIVE');
-        return;
-    }
-    if (PORT == 3000) {
-        arquivo = "C:/Repositorios Git/Simionato/controle de ativo/keys/googlekey.json"
-    } else {
-        arquivo = "keys/googlekey.json"
-    }
-
-    try {
-        var writeStream = fs.createWriteStream(arquivo);
-        writeStream.write(param.parametro)
-        writeStream.end();
-        console.log("Chave Atualizada Com Sucesso!");
-    } catch (error) {
-        console.log(`Erro Na Gravação googlekey, No Servidor ${error}`);
-    }
-
-
-
+  try {
+    var writeStream = fs.createWriteStream(arquivo);
+    writeStream.write(param.parametro);
+    writeStream.end();
+    console.log("Chave Atualizada Com Sucesso!");
+  } catch (error) {
+    console.log(`Erro Na Gravação googlekey, No Servidor ${error}`);
+  }
 }
