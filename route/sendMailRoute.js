1b63d618be13f9761766eba327f0c456d3e609ba
Falconii <falconi.2050@gmail.com>
2024-08-16 15:45:43 -0300

    versao 16 08 C

--- route/sendMailRoute.js
+++ route/sendMailRoute.js
@@ -121,13 +121,11 @@ router.post("/api/enviaremail", async(req, res) => {
     try {
         const { id_empresa, destinatario, assunto, mensagem } = req.body;
 
-        /*  const mailList = [
-                destinatario,
-                "ronie@simionatoauditores.com.br",
-                "daniel.barros@simionatoauditores.com.br",
-            ]; */
-
-        const mailList = [destinatario];
+        const mailList = [
+            destinatario,
+            "ronie@simionatoauditores.com.br",
+            "daniel.barros@simionatoauditores.com.br",
+        ];
 
         // Obtendo o HTML do email com os detalhes do usuário
         const html = await getHtml(id_empresa, destinatario);
