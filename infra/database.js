e9c213e569bc60d0317ea8f523d7236cdb05780d
Marcos Renato Falconi <falconi.2050@gmail.com>
2024-10-05 15:34:04 -0300

    95 10 A

--- infra/database.js
+++ infra/database.js
@@ -10,20 +10,20 @@
 /* fontes gerado automaticamente */
 const pgp = require("pg-promise")();
 
-/* const db = pgp({
+const db = pgp({
   user: "postgres",
   password: "123456",
   host: "localhost",
   port: 5434,
   database: "ativo_homologacao_local",
-}); */
+}); 
 
-const db = pgp({
+/* const db = pgp({
     user: "postgres",
     password: "511fAbAB-12bee6dCd3gDGAA5a4Ag4Ca",
     host: "roundhouse.proxy.rlwy.net",
     port: 59437,
     database: "railway",
-});
+}); */
 
 module.exports = db;
