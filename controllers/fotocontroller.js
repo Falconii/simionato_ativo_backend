56f1868a277fa29865a99ab4ea0d30e129f9e684
Marcos Renato Falconi <falconi.2050@gmail.com>
2025-10-10 13:53:51 -0300

    gggggggg

--- controllers/fotocontroller.js
+++ controllers/fotocontroller.js
@@ -1,11 +1,4 @@
 const fs = require("fs");
-const erroDB = require('../util/userfunctiondb');
-const { google } = require("googleapis");
-const funcoes       = require("../util/googleFuncoes");   // autenticação, upload, etc.
-const inventarioSrv = require("../service/inventarioService");
-const fotoSrv = require("../service/fotoService");
-//const { file } = require("googleapis/build/src/apis/file");
-//const { param } = require("../route/googleRoute");
 
 
 const PORT = process.env.PORT || 3000;
