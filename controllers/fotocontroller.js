1f5c5d8909466b4857508c57a1ec8032d490100e
Marcos Renato Falconi <falconi.2050@gmail.com>
2025-10-10 12:26:52 -0300

    teste 02

--- controllers/fotocontroller.js
+++ controllers/fotocontroller.js
@@ -4,8 +4,8 @@ const { google } = require("googleapis");
 const funcoes       = require("../util/googleFuncoes");   // autenticação, upload, etc.
 const inventarioSrv = require("../service/inventarioService");
 const fotoSrv = require("../service/fotoService");
-const { file } = require("googleapis/build/src/apis/file");
-const { param } = require("../route/googleRoute");
+//const { file } = require("googleapis/build/src/apis/file");
+//const { param } = require("../route/googleRoute");
 
 
 const PORT = process.env.PORT || 3000;
