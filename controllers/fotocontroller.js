c928b8ec328e3f7052867db28e300c0b107f10f2
Marcos Renato Falconi <falconi.2050@gmail.com>
2025-10-10 14:12:45 -0300

    dfghyt

--- controllers/fotocontroller.js
+++ controllers/fotocontroller.js
@@ -65,3 +65,75 @@ async function atualizaFileNameDB_GD(lsFotos){
     return `Total de Fotos Processadas: ${lsFotos.length}`;
 
 }
+
+
+async function processaUploadFotoDisp(req) {
+  console.log("📥 [processaUploadFoto] Iniciando processamento...");
+
+  // Extrai dados
+  const {
+    id_empresa, id_local, id_inventario, id_imobilizado, id_pasta,
+    id_file, id_usuario, data, destaque, obs, localizacao, file_name
+  } = req.body;
+  const file_name_foto_upload = req.file.originalname;
+  fotoSaved = null;
+
+
+  // Autenticação Google
+  const params = await funcoes.loadCredencials(id_empresa);
+  const oauth2Client = funcoes.getoauth2Client(params);
+  const driveService = google.drive({ version: "v3", auth: oauth2Client });
+
+  // Inventário
+  const inventario = await inventarioSrv.getInventario(id_empresa, id_local, id_inventario);
+  const folder_id = inventario.folder_id;
+
+  // Verifica se foto já existe
+  let foto_antiga = await fotoSrv.getFoto(id_empresa, id_local, id_inventario, id_imobilizado, id_pasta, id_file, file_name);
+  if (foto_antiga !== null){
+
+     await fotoSrv.deleteFoto(id_empresa,id_local,id_inventario,id_imobilizado,id_pasta,id_file,file_name); 
+
+     fotoSaved = foto_antiga;
+
+  } else {
+
+      throw new erroDB.UserException('Regra de negócio', [{ tabela: 'UPLOAD FOTO DO DISPOSITIVO', message: `Não Existe Registro Temporário De Foto Do Dispositivo` }]);
+
+  }
+  // Verifica e exclui arquivo anterior do Drive
+  let existeDrive = false;
+  if (id_file && id_file.trim() !== "") {
+    try {
+      const checkFile = await funcoes.existFile(driveService, id_file);
+      existeDrive = checkFile.result;
+    } catch (err) {
+      existeDrive = false;
+    }
+
+    if (existeDrive) {
+      await funcoes.deleteFile(driveService, id_file);
+      console.log("🗑️ Arquivo antigo do Drive excluído");
+    }
+  }
+  // Upload do novo arquivo
+  const saved = await funcoes.saveFile(driveService,file_name_foto_upload, file_name, folder_id);
+
+  foto_antiga.id_file = saved.fileId;
+  foto_antiga.file_name = file_name;
+  foto_antiga.id_pasta = folder_id;
+  foto_antiga.localizacao = 'N';
+  
+  fotoSaved = await fotoSrv.insertFoto(foto_antiga);
+
+   try {
+    fs.unlinkSync(`./fotos/${file_name_foto_upload}`);
+  } catch (err) {
+    console.error("⚠️ Erro ao deletar arquivo local:", err);
+  }
+  
+
+
+  return fotoSaved;
+}
+
